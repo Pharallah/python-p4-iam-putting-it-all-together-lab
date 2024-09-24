dc3cd458b6755b908a9ee0bf98d86b7936ed4af6
Harry San Juan <harrycsanjuan@gmail.com>
2024-09-24 16:25:15 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -8,19 +8,115 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+
+        if not json.get('username'):
+            return {'error': 'Username required'}, 422
+        if not json.get('image_url'):
+            return {'error': 'Image URL required'}, 422
+        if not json.get('bio'):
+            return {'error': 'Bio required'}, 422
+
+        user = User(
+            username=json['username'],
+            image_url=json['image_url'],
+            bio=json['bio']
+        )
+
+        # We set this outside the constructor to trigger the @password_hash.setter (DIFFERENT )
+        user.password_hash = json['password'] 
+
+        db.session.add(user)
+        db.session.commit()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 201
+        
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(
+            User.id == session.get('user_id')
+        ).first()
+
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {
+                'message': 'Unauthorized'
+            }, 401
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict()
+        else:
+            return {'message': 'Invalid Login Credentials'}, 401
 
 class Logout(Resource):
-    pass
-
+    def delete(self):
+        
+        if session['user_id']:
+            session['user_id'] = None
+            return {}, 204
+        else:
+            return {'error': 'Unauthorized'}, 401
+            
 class RecipeIndex(Resource):
-    pass
+    
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+
+        if session['user_id']:
+            recipes_dict = [recipe.to_dict() for recipe in user.recipes]
+            return recipes_dict, 200
+        else:
+            return {'error': 'Unauthorized'}, 401
+        
+    def post(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+
+        if not user:
+            return {'error': 'Unauthorized'}, 401
+        
+        json = request.get_json()
+
+        if not json.get('title'):
+            return {'error': 'Title is required.'}, 422
+        if not json.get('instructions'):
+            return {'error': 'Instructions are required.'}, 422
+        if len(json.get('instructions')) < 50:
+            return {'error': 'Instructions must be at least 50 characters long.'}, 422
+        if not json.get('minutes_to_complete'):
+            return {'error': 'Minutes to complete are required.'}, 422
+        if not isinstance(json.get('minutes_to_complete'), int) or json['minutes_to_complete'] <= 0:
+            return {'error': 'Minutes to complete must be a positive integer.'}, 422
+
+        new_recipe = Recipe(
+            title=json['title'],
+            instructions=json['instructions'],
+            minutes_to_complete=json['minutes_to_complete'],
+            user_id = user.id
+        )
+
+        db.session.add(new_recipe)
+        db.session.commit()
+
+        return new_recipe.to_dict(), 201
+        
+
+        # How do I check if a recipe is valid???
+        # How do I check if a user(created) is not valid??
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
